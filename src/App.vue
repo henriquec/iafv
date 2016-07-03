82111266bdf72d9a6328890fa8dc67ad2f887aab
henriquec <henrique.martins.ramos@gmail.com>
2016-07-02 21:10:43 -0300

    Localization and shell loader

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,57 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+  <div >
+    <left-menu></left-menu>
+    <div class="col-xs-10 app-shell">
+        <router-view></router-view>
+    </div>
+    <div v-if="isLoading" class="shell-loading-div"><i class="fa fa-spin fa-spinner fa-5x"></i></div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
+  import LeftMenu from './components/layout/LeftMenu';
 
-export default {
-  components: {
-    Hello,
-  },
-};
+  export default {
+    data() {
+      return {
+        isLoading: false,
+      };
+    },
+    ready() {
+      this.initializeListeners();
+    },
+    methods: {
+      initializeListeners() {
+        this.$on('loading-start', () => {
+          this.isLoading = true;
+        });
+        this.$on('loading-finish', () => {
+          this.isLoading = false;
+        });
+      },
+    },
+    components: {
+      LeftMenu,
+    },
+  };
 </script>
 
-<style>
-html {
-  height: 100%;
-}
+<style scoped>
+  .shell-loading-div {
+    color: #2EC4B6;
+    background-color: white;
+    opacity: 0.8;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index:99;
+  }
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
+  .shell-loading-div i {
+    position: absolute;
+    left: calc((100% - 65px)/2);
+    top: calc((100% - 65px)/2);
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueI18n from 'vue-i18n';
+import Students from './components/pages/Students';
 import App from './App';
+import locales from './i18n/locales';
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App },
+Vue.use(VueRouter);
+Vue.use(VueI18n);
+
+// set lang
+Vue.config.lang = 'pt-br';
+Vue.config.debug = true;
+
+Object.keys(locales).forEach((lang) => {
+  Vue.locale(lang, locales[lang]);
 });
+
+const router = new VueRouter();
+router.map({
+  '/students/new': {
+    component: Students,
+  },
+});
+
+router.start(App, '#app');
